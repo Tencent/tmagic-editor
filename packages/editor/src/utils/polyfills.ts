cf5e775b84fedca2fc870c74a2c709aa8dd284ea
roymondchen <roymondchen@tencent.com>
2022-05-24 16:35:31 +0800

    feat(editor): 添加globalThis兼容

--- packages/editor/src/utils/polyfills.ts
+++ packages/editor/src/utils/polyfills.ts
@@ -1,22 +1,8 @@
-/*
- * Tencent is pleased to support the open source community by making TMagicEditor available.
- *
- * Copyright (C) 2021 THL A29 Limited, a Tencent company.  All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 // serialize-javascript 依赖的 randombytes 依赖全局的 global 对象，因此此处需添加 global polyfill
-if (typeof (window as any).global === 'undefined') {
+if (typeof global === 'undefined') {
   (window as any).global = window;
+}
+
+if (typeof globalThis === 'undefined') {
+  (window as any).globalThis = window;
 }
