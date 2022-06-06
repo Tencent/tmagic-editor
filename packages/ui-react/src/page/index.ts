cdabe36b9395b5b4c5e565919b5b7a3a74db8d05
roymondchen <roymondchen@tencent.com>
2022-06-06 15:24:59 +0800

    fix(ui): 页面组件无法支持event配置

--- packages/ui-react/src/page/index.ts
+++ packages/ui-react/src/page/index.ts
@@ -20,5 +20,6 @@ import Page from './Page';
 
 export { default as config } from './formConfig';
 export { default as value } from './initValue';
+export { default as event } from './event';
 
 export default Page;
--- packages/ui/src/page/index.ts
+++ packages/ui/src/page/index.ts
@@ -20,5 +20,6 @@ import Page from './src/index.vue';
 
 export { default as config } from './src/formConfig';
 export { default as value } from './src/initValue';
+export { default as event } from './src/event';
 
 export default Page;
