0ae4ccbbf796ef78ad3e3f914d831ccf7b9e55f9
roymondchen <roymondchen@tencent.com>
2022-06-13 21:01:35 +0800

    feat(playground): 优化form/table playground

---
+++ playground/src/components/NavMenu.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="m-editor-nav-menu">
+    <el-button
+      v-for="(item, index) in data"
+      class="menu-item button"
+      :key="index"
+      size="small"
+      text
+      @click="item.handler"
+    >
+      <el-icon><component :is="item.icon"></component></el-icon><span>{{ item.text }}</span>
+    </el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+import { MenuButton } from '@tmagic/editor';
+
+export default defineComponent({
+  name: 'nav-menu',
+
+  props: {
+    data: {
+      type: Array as PropType<MenuButton[]>,
+      default: () => [],
+    },
+  },
+});
+</script>
+
+<style lang="scss">
+.m-editor-nav-menu {
+  justify-content: flex-end;
+  height: 35px;
+}
+</style>
--- playground/src/pages/Table.vue
+++ playground/src/pages/Table.vue
@@ -1,9 +1,30 @@
 <template>
-  <m-table :columns="columns" :data="data" :show-header="true"></m-table>
+  <div style="width: 100%">
+    <nav-menu :data="menu"></nav-menu>
+    <div class="table-content">
+      <m-table class="left-panel" :columns="columns" :data="data" :show-header="true"></m-table>
+      <el-tabs class="right-panel" modelValue="columns">
+        <el-tab-pane label="columns" name="columns">
+          <magic-code-editor class="code-editor-content" :init-values="columns" @save="change"></magic-code-editor>
+        </el-tab-pane>
+        <el-tab-pane label="data" name="data">
+          <magic-code-editor class="code-editor-content" :init-values="data" @save="changeData"></magic-code-editor>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { ElMessage } from 'element-plus';
+
+import { MenuButton } from '@tmagic/editor';
+
+import NavMenu from '../components/NavMenu.vue';
+
+const router = useRouter();
 
 const columns = ref([
   {
@@ -32,4 +53,53 @@ const data = ref([
     ],
   },
 ]);
+
+const menu: MenuButton[] = [
+  {
+    type: 'button',
+    text: 'Editor Playground',
+    handler: () => router.push('/'),
+  },
+  {
+    type: 'button',
+    text: 'Form Playground',
+    handler: () => router.push('form'),
+  },
+];
+
+function change(value: string) {
+  try {
+    // eslint-disable-next-line no-eval
+    columns.value = eval(value);
+    ElMessage.success('更新成功');
+  } catch (e: any) {
+    ElMessage.error(e.message);
+  }
+}
+
+function changeData(value: string) {
+  try {
+    // eslint-disable-next-line no-eval
+    data.value = eval(value);
+    ElMessage.success('更新成功');
+  } catch (e: any) {
+    ElMessage.error(e.message);
+  }
+}
 </script>
+
+<style lang="scss">
+.table-content {
+  display: flex;
+  height: calc(100% - 35px);
+
+  .right-panel,
+  .left-panel {
+    width: 50%;
+  }
+
+  .code-editor-content {
+    height: calc(100vh - 100px);
+  }
+}
+</style>
