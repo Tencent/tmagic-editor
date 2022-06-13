311f400b756571f7a2fd544dcbcdec703425bbc5
roymondchen <roymondchen@tencent.com>
2022-06-13 19:28:12 +0800

    feat(playground): 添加form、table playground入口

---
+++ playground/src/configs/componentGroupList.ts
@@ -0,0 +1,83 @@
+import { FolderOpened, Grid, PictureFilled, SwitchButton, Tickets } from '@element-plus/icons';
+
+export default [
+  {
+    title: '示例容器',
+    items: [
+      {
+        icon: FolderOpened,
+        text: '组',
+        type: 'container',
+      },
+      {
+        icon: FolderOpened,
+        text: '蒙层',
+        type: 'overlay',
+      },
+    ],
+  },
+  {
+    title: '示例组件',
+    items: [
+      {
+        icon: Tickets,
+        text: '文本',
+        type: 'text',
+      },
+      {
+        icon: SwitchButton,
+        text: '按钮',
+        type: 'button',
+      },
+      {
+        icon: PictureFilled,
+        text: '图片',
+        type: 'img',
+      },
+      {
+        icon: Grid,
+        text: '二维码',
+        type: 'qrcode',
+      },
+    ],
+  },
+  {
+    title: '组合',
+    items: [
+      {
+        icon: Tickets,
+        text: '弹窗',
+        data: {
+          type: 'overlay',
+          style: {
+            position: 'fixed',
+            width: '100%',
+            height: '100%',
+            top: 0,
+            left: 0,
+            backgroundColor: 'rgba(0, 0, 0, 0.8)',
+          },
+          name: '弹窗',
+          items: [
+            {
+              type: 'container',
+              style: {
+                position: 'absolute',
+                width: '80%',
+                height: '400',
+                top: '143.87',
+                left: 37.5,
+                backgroundColor: 'rgba(255, 255, 255, 1)',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: '100% 100%',
+              },
+              name: '组',
+              items: [],
+              layout: 'absolute',
+            },
+          ],
+        },
+      },
+    ],
+  },
+];
