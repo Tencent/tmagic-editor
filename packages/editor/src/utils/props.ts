f9eaf8c7543b9887f6d3a0016bba5db570cef5be
roymondchen <roymondchen@tencent.com>
2022-07-27 16:44:30 +0800

    fix(editor): 新增组件id不对

--- packages/editor/src/utils/props.ts
+++ packages/editor/src/utils/props.ts
@@ -237,25 +237,3 @@ export const fillConfig = (config: FormConfig = []) => [
 
 // 默认组件属性表单配置
 export const DEFAULT_CONFIG: FormConfig = fillConfig([]);
-
-/**
- * 获取默认属性配置
- * @param type 组件类型
- * @returns Object
- */
-export const getDefaultPropsValue = (type: string, id: string) =>
-  ['page', 'container'].includes(type)
-    ? {
-        type,
-        id,
-        layout: 'absolute',
-        style: {},
-        name: type,
-        items: [],
-      }
-    : {
-        type,
-        id,
-        style: {},
-        name: type,
-      };
