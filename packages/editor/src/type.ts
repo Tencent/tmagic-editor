471b0e767a8306f4232b9a5ae9b967c8072c3ec7
wangminghua <735161977@qq.com>
2022-08-05 19:25:11 +0800

    fix(editor): 添加组件粘贴操作支持偏移量

--- packages/editor/src/type.ts
+++ packages/editor/src/type.ts
@@ -135,6 +135,14 @@ export interface AddMNode {
 export interface PastePosition {
   left?: number;
   top?: number;
+  /**
+   * 粘贴位置X方向偏移量
+   */
+  offsetX?: number;
+  /**
+   * 粘贴位置Y方向偏移量
+   */
+  offsetY?: number;
 }
 
 /**
