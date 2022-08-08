ce9d9821a70e5443a1bc5b9b0b1c984ec4ad8e2f
roymondchen <roymondchen@tencent.com>
2022-08-08 16:12:35 +0800

    build: 修改runtime  publicDir

--- playground/src/pages/Editor.vue
+++ playground/src/pages/Editor.vue
@@ -15,23 +15,18 @@
       :stage-rect="stageRect"
     >
       <template #workspace-content>
-        <device-group v-model="stageRect"></device-group>
+        <DeviceGroup v-model="stageRect"></DeviceGroup>
       </template>
     </m-editor>
 
     <el-dialog v-model="previewVisible" destroy-on-close :width="375" custom-class="pre-viewer" title="预览">
-      <iframe
-        v-if="previewVisible"
-        width="100%"
-        height="817"
-        :src="`${VITE_RUNTIME_PATH}/page/index.html?localPreview=1&page=${editor?.editorService.get('page').id}`"
-      ></iframe>
+      <iframe v-if="previewVisible" width="100%" height="817" :src="previewUrl"></iframe>
     </el-dialog>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, toRaw } from 'vue';
+<script lang="ts" setup>
+import { computed, ref, toRaw } from 'vue';
 import { useRouter } from 'vue-router';
 import { Coin, Connection, Document } from '@element-plus/icons-vue';
 import { ElMessage, ElMessageBox } from 'element-plus';
@@ -47,145 +42,124 @@ import DeviceGroup from '../components/DeviceGroup.vue';
 import componentGroupList from '../configs/componentGroupList';
 import dsl from '../configs/dsl';
 
-const { VITE_RUNTIME_PATH } = import.meta.env;
+const { VITE_RUNTIME_PATH, VITE_ENTRY_PATH } = import.meta.env;
 
-export default defineComponent({
-  name: 'EditorApp',
+const runtimeUrl = `${VITE_RUNTIME_PATH}/playground/index.html`;
+const router = useRouter();
+const editor = ref<InstanceType<typeof TMagicEditor>>();
+const previewVisible = ref(false);
+const value = ref(dsl);
+const defaultSelected = ref(dsl.items[0].id);
+const propsValues = ref<Record<string, any>>({});
+const propsConfigs = ref<Record<string, any>>({});
+const eventMethodList = ref<Record<string, any>>({});
+const stageRect = ref();
 
-  components: { DeviceGroup },
+const previewUrl = computed(
+  () => `${VITE_RUNTIME_PATH}/page/index.html?localPreview=1&page=${editor.value?.editorService.get('page').id}`,
+);
 
-  setup() {
-    const router = useRouter();
-    const editor = ref<InstanceType<typeof TMagicEditor>>();
-    const previewVisible = ref(false);
-    const value = ref(dsl);
-    const defaultSelected = ref(dsl.items[0].id);
-    const propsValues = ref<Record<string, any>>({});
-    const propsConfigs = ref<Record<string, any>>({});
-    const eventMethodList = ref<Record<string, any>>({});
-
-    const save = () => {
-      localStorage.setItem(
-        'magicDSL',
-        serialize(toRaw(value.value), {
-          space: 2,
-          unsafe: true,
-        }).replace(/"(\w+)":\s/g, '$1: '),
-      );
-      editor.value?.editorService.resetModifiedNodeId();
-    };
-
-    const menu: MenuBarData = {
-      left: [
-        {
-          type: 'text',
-          text: '魔方',
-        },
-      ],
-      center: ['delete', 'undo', 'redo', 'guides', 'rule', 'zoom'],
-      right: [
-        {
-          type: 'button',
-          text: 'Form Playground',
-          handler: () => router.push('form'),
-        },
-        {
-          type: 'button',
-          text: 'Table Playground',
-          handler: () => router.push('table'),
-        },
-        '/',
-        {
-          type: 'button',
-          text: '预览',
-          icon: Connection,
-          handler: async (services) => {
-            if (services?.editorService.get<Map<Id, Id>>('modifiedNodeIds').size > 0) {
-              try {
-                await ElMessageBox.confirm('有修改未保存，是否先保存再预览', '提示', {
-                  confirmButtonText: '保存并预览',
-                  cancelButtonText: '预览',
-                  type: 'warning',
-                });
-                save();
-                ElMessage.success('保存成功');
-              } catch (e) {
-                console.error(e);
-              }
-            }
-            previewVisible.value = true;
-          },
-        },
-        {
-          type: 'button',
-          text: '保存',
-          icon: Coin,
-          handler: () => {
+const menu: MenuBarData = {
+  left: [
+    {
+      type: 'text',
+      text: '魔方',
+    },
+  ],
+  center: ['delete', 'undo', 'redo', 'guides', 'rule', 'zoom'],
+  right: [
+    {
+      type: 'button',
+      text: 'Form Playground',
+      handler: () => router.push('form'),
+    },
+    {
+      type: 'button',
+      text: 'Table Playground',
+      handler: () => router.push('table'),
+    },
+    '/',
+    {
+      type: 'button',
+      text: '预览',
+      icon: Connection,
+      handler: async (services) => {
+        if (services?.editorService.get<Map<Id, Id>>('modifiedNodeIds').size > 0) {
+          try {
+            await ElMessageBox.confirm('有修改未保存，是否先保存再预览', '提示', {
+              confirmButtonText: '保存并预览',
+              cancelButtonText: '预览',
+              type: 'warning',
+            });
             save();
             ElMessage.success('保存成功');
-          },
-        },
-        '/',
-        {
-          type: 'button',
-          icon: Document,
-          tooltip: '源码',
-          handler: (service) => service?.uiService.set('showSrc', !service?.uiService.get('showSrc')),
-        },
-      ],
-    };
-
-    asyncLoadJs(`${VITE_RUNTIME_PATH}/config-entry/index.umd.js`).then(() => {
-      propsConfigs.value = (globalThis as any).magicPresetConfigs;
-    });
-    asyncLoadJs(`${VITE_RUNTIME_PATH}/value-entry/index.umd.js`).then(() => {
-      propsValues.value = (globalThis as any).magicPresetValues;
-    });
-    asyncLoadJs(`${VITE_RUNTIME_PATH}/event-entry/index.umd.js`).then(() => {
-      eventMethodList.value = (globalThis as any).magicPresetEvents;
-    });
-
-    save();
-
-    return {
-      VITE_RUNTIME_PATH,
-
-      editor,
-      menu,
-      value,
-      defaultSelected,
-      propsValues,
-      propsConfigs,
-      eventMethodList,
-      stageRect: ref(),
-
-      previewVisible,
-
-      runtimeUrl: `${VITE_RUNTIME_PATH}/playground/index.html`,
-
-      componentGroupList,
-
-      moveableOptions: (core?: StageCore): MoveableOptions => {
-        const options: MoveableOptions = {};
-        const id = core?.dr?.target?.id;
-
-        if (!id || !editor.value) return options;
-
-        const node = editor.value.editorService.getNodeById(id);
-
-        if (!node) return options;
-
-        const isPage = node.type === NodeType.PAGE;
-
-        options.draggable = !isPage;
-        options.resizable = !isPage;
-        options.rotatable = !isPage;
-
-        return options;
+          } catch (e) {
+            console.error(e);
+          }
+        }
+        previewVisible.value = true;
       },
-    };
-  },
+    },
+    {
+      type: 'button',
+      text: '保存',
+      icon: Coin,
+      handler: () => {
+        save();
+        ElMessage.success('保存成功');
+      },
+    },
+    '/',
+    {
+      type: 'button',
+      icon: Document,
+      tooltip: '源码',
+      handler: (service) => service?.uiService.set('showSrc', !service?.uiService.get('showSrc')),
+    },
+  ],
+};
+
+const moveableOptions = (core?: StageCore): MoveableOptions => {
+  const options: MoveableOptions = {};
+  const id = core?.dr?.target?.id;
+
+  if (!id || !editor.value) return options;
+
+  const node = editor.value.editorService.getNodeById(id);
+
+  if (!node) return options;
+
+  const isPage = node.type === NodeType.PAGE;
+
+  options.draggable = !isPage;
+  options.resizable = !isPage;
+  options.rotatable = !isPage;
+
+  return options;
+};
+
+const save = () => {
+  localStorage.setItem(
+    'magicDSL',
+    serialize(toRaw(value.value), {
+      space: 2,
+      unsafe: true,
+    }).replace(/"(\w+)":\s/g, '$1: '),
+  );
+  editor.value?.editorService.resetModifiedNodeId();
+};
+
+asyncLoadJs(`${VITE_ENTRY_PATH}/config/index.umd.js`).then(() => {
+  propsConfigs.value = (globalThis as any).magicPresetConfigs;
 });
+asyncLoadJs(`${VITE_ENTRY_PATH}/value/index.umd.js`).then(() => {
+  propsValues.value = (globalThis as any).magicPresetValues;
+});
+asyncLoadJs(`${VITE_ENTRY_PATH}/event/index.umd.js`).then(() => {
+  eventMethodList.value = (globalThis as any).magicPresetEvents;
+});
+
+save();
 </script>
 
 <style lang="scss">
