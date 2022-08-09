3cab894c6f7e7333cb165fc53e7dff3c259d1513
roymondchen <roymondchen@tencent.com>
2022-08-09 19:53:34 +0800

    fix(ui-vue2): button,container,text formconfig补充完整

--- packages/ui-vue2/src/container/formConfig.ts
+++ packages/ui-vue2/src/container/formConfig.ts
@@ -16,4 +16,15 @@
  * limitations under the License.
  */
 
-export default [];
+export default [
+  {
+    name: 'layout',
+    text: '容器布局',
+    type: 'select',
+    defaultValue: 'absolute',
+    options: [
+      { value: 'absolute', text: '绝对定位' },
+      { value: 'relative', text: '流式布局' },
+    ],
+  },
+];
