4efd7d1400b93db58ecbfd141d4e6232b12f9670
i33 <78162524@qq.com>
2022-08-18 18:18:08 +0800

    fix(editor): 解决UISelect控制台警告
    tooltip使用时，如果直接子元素有v-if, 会造成控制台警告`[ElOnlyChild] no valid child node found`,添加一个固定元素，避免警告。

--- packages/editor/src/fields/UISelect.vue
+++ packages/editor/src/fields/UISelect.vue
@@ -4,7 +4,16 @@
   </div>
   <div class="m-fields-ui-select" v-else @click="startSelect" style="display: flex">
     <el-tooltip content="清除">
-      <el-button v-if="val" style="padding: 0" type="danger" :icon="Close" text @click.stop="deleteHandler"></el-button>
+      <span>
+        <el-button
+          v-if="val"
+          style="padding: 0"
+          type="danger"
+          :icon="Close"
+          text
+          @click.stop="deleteHandler"
+        ></el-button>
+      </span>
     </el-tooltip>
     <el-tooltip :content="val ? toName + '_' + val : '点击此处选择'">
       <el-button text style="padding: 0; margin: 0">{{ val ? toName + '_' + val : '点击此处选择' }}</el-button>
