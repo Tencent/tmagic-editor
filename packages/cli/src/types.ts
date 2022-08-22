221336f634b78f16def6279bce90c8c38a196cc7
roymondchen <roymondchen@tencent.com>
2022-08-22 15:00:25 +0800

    docs: tmagic/cli

--- packages/cli/src/types.ts
+++ packages/cli/src/types.ts
@@ -34,7 +34,9 @@ export interface EntryFile {
 }
 
 export interface NpmConfig {
+  /** npm镜像代理 */
   registry?: string;
+  /** pnpm | npm | yarn */
   client?: 'npm' | 'yarn' | 'pnpm';
 }
 
@@ -49,9 +51,12 @@ export interface ModuleMainFilePath {
 export interface UserConfig {
   source: string;
   temp: string;
+  /** 组件目录或者npm包名 */
   packages: (string | Record<string, string>)[];
+  /** 组件文件后缀名，例如vue文件为.vue，tsx文件为.tsx，普通js文件则为.js */
   componentFileAffix: string;
   cleanTemp: boolean;
+  /** npm 配置，用于当packages配置有npm包名时，可以自动安装npm包 */
   npmConfig?: NpmConfig;
   onInit?: (app: Core) => ModuleMainFilePath | Promise<ModuleMainFilePath>;
   onPrepare?: (app: Core) => void;
