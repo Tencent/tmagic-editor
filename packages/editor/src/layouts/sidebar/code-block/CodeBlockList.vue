452c80d829e3e127a51ac9c5cb1356bc4eafba61
parisma <parisma@tencent.com>
2022-09-07 16:38:10 +0800

    feat(editor): 代码块功能新增slot

--- packages/editor/src/layouts/sidebar/code-block/CodeBlockList.vue
+++ packages/editor/src/layouts/sidebar/code-block/CodeBlockList.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="m-editor-code-block-list">
-    <slot name="code-block-panel-header"></slot>
-    <div class="create-code-button">
-      <el-button type="primary" size="small" @click="createCodeBlock">新增代码块</el-button>
-    </div>
-    <el-divider class="divider" />
+    <slot name="code-block-panel-header">
+      <div class="create-code-button">
+        <el-button type="primary" size="small" @click="createCodeBlock">新增代码块</el-button>
+      </div>
+      <el-divider class="divider" />
+    </slot>
 
     <!-- 代码块列表 -->
     <div class="list-container" v-if="state.codeBlockMap">
       <div v-for="(value, key) in state.codeBlockMap" :key="key">
         <div class="list-item">
           <div class="code-name">{{ value.name }}（{{ key }}）</div>
-          <el-tooltip class="box-item" effect="dark" content="编辑代码" placement="top">
-            <el-icon class="edit-icon" @click="editCode(key)"><EditPen /></el-icon>
-          </el-tooltip>
+          <div class="right-tool">
+            <el-tooltip effect="dark" content="编辑代码" placement="top">
+              <el-icon class="edit-icon" @click="editCode(key)"><Edit /></el-icon>
+            </el-tooltip>
+            <slot name="code-block-panel-tool"></slot>
+          </div>
         </div>
       </div>
     </div>
@@ -25,7 +29,7 @@
 
 <script lang="ts" setup>
 import { inject, reactive, watchEffect } from 'vue';
-import { EditPen } from '@element-plus/icons-vue';
+import { Edit } from '@element-plus/icons-vue';
 
 import type { CodeBlockConfig, Services, State } from '../../../type';
 
