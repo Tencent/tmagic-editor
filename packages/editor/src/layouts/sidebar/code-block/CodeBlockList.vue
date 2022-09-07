c9a64b4b87d8203a30701c8cca15e3f070c9da63
parisma <parisma@tencent.com>
2022-09-07 14:45:38 +0800

    feat(editor): 新增,编辑代码块完成

--- packages/editor/src/layouts/sidebar/code-block/CodeBlockList.vue
+++ packages/editor/src/layouts/sidebar/code-block/CodeBlockList.vue
@@ -7,15 +7,78 @@
     <el-divider class="divider" />
 
     <!-- 代码块列表 -->
-    <div class="list-container"></div>
+    <div class="list-container" v-if="state.codeBlockMap">
+      <div v-for="(value, key) in state.codeBlockMap" :key="key">
+        <div class="list-item">
+          <div class="code-name">{{ value.name }}（{{ key }}）</div>
+          <el-tooltip class="box-item" effect="dark" content="编辑代码" placement="top">
+            <el-icon class="edit-icon" @click="editCode(key)"><EditPen /></el-icon>
+          </el-tooltip>
+        </div>
+      </div>
+    </div>
 
     <!-- 代码块编辑区 -->
-    <code-block-editor />
+    <code-block-editor :state="state" @close="closePanel"></code-block-editor>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { inject, reactive, watchEffect } from 'vue';
+import { EditPen } from '@element-plus/icons-vue';
+
+import type { CodeBlockConfig, Services, State } from '../../../type';
+
 import codeBlockEditor from './CodeBlockEditor.vue';
-import useCodeBlock from './useCodeBlock';
-const { createCodeBlock } = useCodeBlock();
+
+const state = reactive<State>({
+  isShowCodeBlockEditor: false,
+  codeConfig: null,
+  codeBlockMap: null,
+});
+const services = inject<Services>('services');
+
+// 新增代码块
+const createCodeBlock = () => {
+  const config: CodeBlockConfig = {
+    id: getUniqueId(),
+    name: '代码块',
+    content: `(app) => {\n  // place your code here\n}`,
+  };
+  showCodeEditor(config);
+};
+
+// 展示代码编辑区
+const showCodeEditor = (config: CodeBlockConfig) => {
+  state.codeConfig = config;
+  showPanel();
+};
+
+// 打开代码块面板
+const showPanel = () => (state.isShowCodeBlockEditor = true);
+
+// 关闭代码块面板
+const closePanel = () => (state.isShowCodeBlockEditor = false);
+
+// 生成代码块唯一id
+const getUniqueId = () => (Date.now().toString(36) + Math.random().toString(36).substring(2)).padEnd(19, '0');
+
+// 编辑代码块
+const editCode = (key: string) => {
+  if (!state.codeBlockMap) return;
+  // 获取对应Id的代码块
+  const currentCode = state.codeBlockMap[key];
+  const config: CodeBlockConfig = {
+    id: key,
+    ...currentCode,
+  };
+  showCodeEditor(config);
+};
+
+watchEffect(() => {
+  const { editorService } = services || {};
+  if (!editorService) return;
+  const root = editorService.get('root');
+  state.codeBlockMap = root?.method || null;
+});
 </script>
