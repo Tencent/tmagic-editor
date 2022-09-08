bc7c1c392d7996d1df92ba20de96077dc8f65927
roymondchen <roymondchen@tencent.com>
2022-09-08 12:43:23 +0800

    style(editor): icon组件图标样式调整

--- packages/editor/src/components/Icon.vue
+++ packages/editor/src/components/Icon.vue
@@ -1,8 +1,10 @@
 <template>
-  <el-icon v-if="!icon"><Edit></Edit></el-icon>
-  <el-icon v-else-if="typeof icon === 'string' && icon.startsWith('http')"><img :src="icon" /></el-icon>
-  <i v-else-if="typeof icon === 'string'" :class="icon"></i>
-  <el-icon v-else><component :is="toRaw(icon)"></component></el-icon>
+  <el-icon v-if="!icon" class="magic-editor-icon"><Edit></Edit></el-icon>
+  <el-icon v-else-if="typeof icon === 'string' && icon.startsWith('http')" class="magic-editor-icon"
+    ><img :src="icon"
+  /></el-icon>
+  <i v-else-if="typeof icon === 'string'" class="magic-editor-icon" :class="icon"></i>
+  <el-icon v-else class="magic-editor-icon"><component :is="toRaw(icon)"></component></el-icon>
 </template>
 
 <script lang="ts" setup>
