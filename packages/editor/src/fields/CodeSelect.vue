90282f1c08094c79906845a6590c5d29ac5686a2
parisma <parisma@tencent.com>
2022-09-14 17:53:12 +0800

    fix(editor): 解决修改代码名称后已选择的select tag不更新的问题,优化一些样式

--- packages/editor/src/fields/CodeSelect.vue
+++ packages/editor/src/fields/CodeSelect.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="m-fields-code-select">
+  <div class="m-fields-code-select" :key="fieldKey">
     <m-fields-select
       :config="selectConfig"
       :model="model"
@@ -20,7 +20,7 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, defineEmits, defineProps, inject, ref } from 'vue';
+import { computed, defineEmits, defineProps, inject, ref, watchEffect } from 'vue';
 import { View } from '@element-plus/icons-vue';
 import { map } from 'lodash-es';
 
@@ -62,22 +62,47 @@ const selectConfig = computed(() => {
     ...props.config.selectConfig,
   };
 });
+const fieldKey = ref('');
+const combineIds = ref<string[]>([]);
 
-const combineIds = ref();
+watchEffect(async () => {
+  console.log('--combineIds.value--', combineIds.value);
+  if (!combineIds.value) return;
+  const combineNames = await Promise.all(
+    combineIds.value.map(async (id) => {
+      const { name = '' } = (await services?.codeBlockService.getCodeContentById(id)) || {};
+      return name;
+    }),
+  );
+  fieldKey.value = combineNames.join('-');
+});
+// watch(
+//   () => services?.codeBlockService.getCodeEditorShowStatus(),
+//   (value, oldValue) => {
+//     if (oldValue && !value) {
+//       // 由打开到关闭
+//       fieldKey.value = '222'
+//       // const selectedValue = props.model[props.name];
+//       // props.model[props.name] = null;
+//       // props.model[props.name] = selectedValue;
+//     }
+//   },
+//   { immediate: true },
+// );
 
 const changeHandler = async (value: any) => {
   // 记录组件与代码块的绑定关系
   const { id = '' } = services?.editorService.get('node') || {};
   await services?.codeBlockService.setCompRelation(id, value);
+  combineIds.value = props.model[props.name];
+  if (typeof props.model[props.name] === 'string') {
+    combineIds.value = [props.model[props.name]];
+  }
   emit('change', value);
 };
 
 const viewHandler = async () => {
   await services?.codeBlockService.setMode(EditorMode.LIST);
-  combineIds.value = props.model[props.name];
-  if (typeof props.model[props.name] === 'string') {
-    combineIds.value = [props.model[props.name]];
-  }
   await services?.codeBlockService.setCombineIds(combineIds.value);
   services?.codeBlockService.setCodeEditorContent(true, combineIds.value[0]);
 };
