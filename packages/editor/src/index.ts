eae776990cd8e8df6492b6a3ab89a6ee26edc617
roymondchen <roymondchen@tencent.com>
2022-11-08 16:21:47 +0800

    fix(editor): Layout 组件与Layout ts type 重名，将组件改为LayoutContainer

--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -48,7 +48,7 @@ export { default as PropsPanel } from './layouts/PropsPanel.vue';
 export { default as ToolButton } from './components/ToolButton.vue';
 export { default as ContentMenu } from './components/ContentMenu.vue';
 export { default as Icon } from './components/Icon.vue';
-export { default as Layout } from './components/Layout.vue';
+export { default as LayoutContainer } from './components/Layout.vue';
 
 const defaultInstallOpt: InstallOptions = {
   // @todo, 自定义图片上传方法等编辑器依赖的外部选项
