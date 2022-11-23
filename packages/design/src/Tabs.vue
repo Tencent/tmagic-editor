47e851ce5e1e9a60672b3854229552ff24989188
roymondchen <roymondchen@tencent.com>
2022-11-23 15:40:02 +0800

    fix(design): tabs支持modelValue

--- packages/design/src/Tabs.vue
+++ packages/design/src/Tabs.vue
@@ -5,6 +5,7 @@
     @tab-click="tabClickHandler"
     @tab-add="onTabAdd"
     @tab-remove="onTabRemove"
+    @update:model-value="updateModelName"
   >
     <template #default>
       <slot></slot>
@@ -21,13 +22,14 @@ const props = defineProps<{
   type?: string;
   editable?: boolean;
   tabPosition?: string;
+  modelValue?: string | number;
 }>();
 
 const uiComponent = getConfig('components').tabs;
 
 const uiProps = computed(() => uiComponent.props(props));
 
-const emit = defineEmits(['tab-click', 'tab-add', 'tab-remove']);
+const emit = defineEmits(['tab-click', 'tab-add', 'tab-remove', 'update:model-value']);
 
 const tabClickHandler = (...args: any[]) => {
   emit('tab-click', ...args);
@@ -39,5 +41,9 @@ const onTabAdd = (...args: any[]) => {
 
 const onTabRemove = (...args: any[]) => {
   emit('tab-remove', ...args);
+};
+
+const updateModelName = (...args: any[]) => {
+  emit('update:model-value', ...args);
 };
 </script>
