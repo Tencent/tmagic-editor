0f073e9441bb40ada5702e62e732bb93b5f5e188
roymondchen <roymondchen@tencent.com>
2022-12-22 11:27:49 +0800

    chore(magic-admin): 更新tmagic

--- magic-admin/web/src/main.ts
+++ magic-admin/web/src/main.ts
@@ -19,7 +19,9 @@
 import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
 
+import TMagicDesign from '@tmagic/design';
 import MagicEditor, { editorService } from '@tmagic/editor';
+import MagicElementPlusAdapter from '@tmagic/element-plus-adapter';
 import MagicForm from '@tmagic/form';
 
 import App from '@src/app.vue';
@@ -33,6 +35,7 @@ import '@tmagic/form/dist/style.css';
 
 const app = createApp(App);
 app.use(ElementPlus);
+app.use(TMagicDesign, MagicElementPlusAdapter);
 app.use(MagicEditor);
 editorService.usePlugin(editorPlugin);
 app.use(MagicForm);
