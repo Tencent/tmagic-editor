a4fc95775eab88ecb5850f824ae5bf7889e951e4
roymondchen <roymondchen@tencent.com>
2024-03-25 14:52:11 +0800

    fix(editor): element-plus更新到2.6后表单项清除后是undefined，除了string外其他统一改成null

--- packages/editor/src/services/editor.ts
+++ packages/editor/src/services/editor.ts
@@ -533,7 +533,15 @@ class Editor extends BaseService {
 
     let newConfig = await this.toggleFixedPosition(toRaw(config), node, root);
 
-    newConfig = mergeWith(cloneDeep(node), newConfig, (objValue, srcValue) => {
+    newConfig = mergeWith(cloneDeep(node), newConfig, (objValue, srcValue, key) => {
+      if (typeof srcValue === 'undefined' && Object.hasOwn(newConfig, key)) {
+        if (typeof objValue === 'string') {
+          return '';
+        }
+
+        return null;
+      }
+
       if (isObject(srcValue) && Array.isArray(objValue)) {
         // 原来的配置是数组，新的配置是对象，则直接使用新的值
         return srcValue;
