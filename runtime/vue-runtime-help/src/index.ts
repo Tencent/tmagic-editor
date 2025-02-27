2114b71d47d591449ec1eff9f2c349cf8ef0d361
roymondchen <roymondchen@tencent.com>
2025-02-27 19:28:08 +0800

    style(vue-runtime-help): 添加UserRenderFunction定义

--- runtime/vue-runtime-help/src/index.ts
+++ runtime/vue-runtime-help/src/index.ts
@@ -1,4 +1,20 @@
+import { h } from 'vue-demi';
+
+import type { MComponent } from '@tmagic/core';
+
 export * from './hooks/use-editor-dsl';
 export * from './hooks/use-dsl';
 export * from './hooks/use-app';
 export { useComponent } from './hooks/use-component';
+
+export interface userRenderFunctionOptions {
+  h: typeof h;
+  type: Parameters<typeof h>[0];
+  props: any;
+  attrs: any;
+  className: string | string[];
+  style: any;
+  config: MComponent;
+}
+
+export type UserRenderFunction = (options: userRenderFunctionOptions) => any;
