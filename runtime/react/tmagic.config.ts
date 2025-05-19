79f65fb51669640414e2e5ca376e78945731242a
roymondchen <roymondchen@tencent.com>
2025-05-19 16:16:03 +0800

    chore(ui): 将ui/ui-react从packages移入runtime中，不再跟随packags发布

--- runtime/react/tmagic.config.ts
+++ runtime/react/tmagic.config.ts
@@ -3,6 +3,6 @@ import path from 'path';
 import { defineConfig } from '@tmagic/cli';
 
 export default defineConfig({
-  packages: [path.join(__dirname, '../../packages/ui-react')],
+  packages: [path.join(__dirname, '../ui-react')],
   componentFileAffix: '.tsx',
 });
--- runtime/vue3/tmagic.config.ts
+++ runtime/vue3/tmagic.config.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { defineConfig } from '@tmagic/cli';
 
 export default defineConfig({
-  packages: [path.join(__dirname, '../../packages/ui')],
+  packages: [path.join(__dirname, '../ui')],
   componentFileAffix: '.vue',
   dynamicImport: true,
 });
