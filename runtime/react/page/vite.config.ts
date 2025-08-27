001f2b6c9e93edb0457b7be6efe0da3fe55d1d81
roymondchen <roymondchen@tencent.com>
2025-08-27 19:18:17 +0800

    build(runtime): escape % if URI malformed

--- runtime/react/page/vite.config.ts
+++ runtime/react/page/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 
 import baseConfig from '../vite.config';
 
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 export default defineConfig({
   ...baseConfig,
 
@@ -15,5 +18,15 @@ export default defineConfig({
     emptyOutDir: false,
     sourcemap: true,
     outDir: '../dist/page',
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : '';
+          return driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '');
+        },
+      },
+    },
   },
 });
